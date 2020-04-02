6c623f143a4d2ea8aa75cb6d6ffa7b87979ba376
britsk <thienminh.npn@gmail.com>
2020-04-02 22:49:43 +0800

    revised the imu_factor.h for some mistakes by the authors and better readability

--- vins_estimator/src/factor/imu_factor.h
+++ vins_estimator/src/factor/imu_factor.h
@@ -28,19 +28,19 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
     virtual bool Evaluate(double const *const *parameters, double *residuals, double **jacobians) const
     {
 
-        Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
+        Eigen::Vector3d    Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
         Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);
 
-        Eigen::Vector3d Vi(parameters[1][0], parameters[1][1], parameters[1][2]);
-        Eigen::Vector3d Bai(parameters[1][3], parameters[1][4], parameters[1][5]);
-        Eigen::Vector3d Bgi(parameters[1][6], parameters[1][7], parameters[1][8]);
+        Eigen::Vector3d    Vi(parameters[1][0], parameters[1][1], parameters[1][2]);
+        Eigen::Vector3d   Bai(parameters[1][3], parameters[1][4], parameters[1][5]);
+        Eigen::Vector3d   Bgi(parameters[1][6], parameters[1][7], parameters[1][8]);
 
-        Eigen::Vector3d Pj(parameters[2][0], parameters[2][1], parameters[2][2]);
+        Eigen::Vector3d    Pj(parameters[2][0], parameters[2][1], parameters[2][2]);
         Eigen::Quaterniond Qj(parameters[2][6], parameters[2][3], parameters[2][4], parameters[2][5]);
 
-        Eigen::Vector3d Vj(parameters[3][0], parameters[3][1], parameters[3][2]);
-        Eigen::Vector3d Baj(parameters[3][3], parameters[3][4], parameters[3][5]);
-        Eigen::Vector3d Bgj(parameters[3][6], parameters[3][7], parameters[3][8]);
+        Eigen::Vector3d    Vj(parameters[3][0], parameters[3][1], parameters[3][2]);
+        Eigen::Vector3d   Baj(parameters[3][3], parameters[3][4], parameters[3][5]);
+        Eigen::Vector3d   Bgj(parameters[3][6], parameters[3][7], parameters[3][8]);
 
 //Eigen::Matrix<double, 15, 15> Fd;
 //Eigen::Matrix<double, 15, 12> Gd;
@@ -98,7 +98,7 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
             {
                 ROS_WARN("numerical unstable in preintegration");
                 //std::cout << pre_integration->jacobian << std::endl;
-///                ROS_BREAK();
+                // ROS_BREAK();
             }
 
             if (jacobians[0])
@@ -106,20 +106,42 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
                 Eigen::Map<Eigen::Matrix<double, 15, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
                 jacobian_pose_i.setZero();
 
-                jacobian_pose_i.block<3, 3>(O_P, O_P) = -Qi.inverse().toRotationMatrix();
-                jacobian_pose_i.block<3, 3>(O_P, O_R) = Utility::skewSymmetric(Qi.inverse() * (0.5 * G * sum_dt * sum_dt + Pj - Pi - Vi * sum_dt));
 
-#if 0
-            jacobian_pose_i.block<3, 3>(O_R, O_R) = -(Qj.inverse() * Qi).toRotationMatrix();
-#else
-                Eigen::Quaterniond corrected_delta_q = pre_integration->delta_q * Utility::deltaQ(dq_dbg * (Bgi - pre_integration->linearized_bg));
-                jacobian_pose_i.block<3, 3>(O_R, O_R) = -(Utility::Qright(corrected_delta_q)
-                                                          *Utility::Qleft(Qj.inverse() * Qi)).bottomRightCorner<3, 3>();
-#endif
+                // Jacobians from preintegrated position (alpha)-----------------------------------------------
+
+                jacobian_pose_i.block<3, 3>(O_P, O_P) = -Qi.inverse().toRotationMatrix();
+                jacobian_pose_i.block<3, 3>(O_P, O_R) =  Utility::skewSymmetric(
+                                                                                 Qi.inverse() 
+                                                                                 *
+                                                                                 (
+                                                                                   0.5 * G * sum_dt * sum_dt
+                                                                                       + Pj - Pi
+                                                                                       - Vi * sum_dt
+                                                                                 )
+                                                                               );
+
+
+                // Jacobians from preintegrated velocity (beta)------------------------------------------------
 
                 jacobian_pose_i.block<3, 3>(O_V, O_R) = Utility::skewSymmetric(Qi.inverse() * (G * sum_dt + Vj - Vi));
 
+
+                // Jacobians from preintegrated quaternion (gamma)---------------------------------------------
+
+                Eigen::Quaterniond corrected_delta_q = pre_integration->delta_q
+                                                       *
+                                                       Utility::deltaQ(dq_dbg * (Bgi - pre_integration->linearized_bg));
+                jacobian_pose_i.block<3, 3>(O_R, O_R) = -(
+                                                           Utility::Qright(corrected_delta_q)
+                                                           *
+                                                           Utility::Qleft(Qj.inverse() * Qi)
+                                                         ).bottomRightCorner<3, 3>();
+
+
+                // Applying the weightage----------------------------------------------------------------------
+
                 jacobian_pose_i = sqrt_info * jacobian_pose_i;
+
 
                 if (jacobian_pose_i.maxCoeff() > 1e8 || jacobian_pose_i.minCoeff() < -1e8)
                 {
@@ -132,48 +154,78 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
             {
                 Eigen::Map<Eigen::Matrix<double, 15, 9, Eigen::RowMajor>> jacobian_speedbias_i(jacobians[1]);
                 jacobian_speedbias_i.setZero();
-                jacobian_speedbias_i.block<3, 3>(O_P, O_V - O_V) = -Qi.inverse().toRotationMatrix() * sum_dt;
+
+                // Jacobians from preintegrated position (alpha)-----------------------------------------------
+
+                jacobian_speedbias_i.block<3, 3>(O_P, O_V - O_V)  = -Qi.inverse().toRotationMatrix() * sum_dt;
                 jacobian_speedbias_i.block<3, 3>(O_P, O_BA - O_V) = -dp_dba;
                 jacobian_speedbias_i.block<3, 3>(O_P, O_BG - O_V) = -dp_dbg;
 
-#if 0
-            jacobian_speedbias_i.block<3, 3>(O_R, O_BG - O_V) = -dq_dbg;
-#else
-                //Eigen::Quaterniond corrected_delta_q = pre_integration->delta_q * Utility::deltaQ(dq_dbg * (Bgi - pre_integration->linearized_bg));
-                //jacobian_speedbias_i.block<3, 3>(O_R, O_BG - O_V) = -Utility::Qleft(Qj.inverse() * Qi * corrected_delta_q).bottomRightCorner<3, 3>() * dq_dbg;
-                jacobian_speedbias_i.block<3, 3>(O_R, O_BG - O_V) = -Utility::Qleft(Qj.inverse() * Qi
-                                                                                    * pre_integration->delta_q
-                                                                                   ).bottomRightCorner<3, 3>() * dq_dbg;
-#endif
 
-                jacobian_speedbias_i.block<3, 3>(O_V, O_V - O_V) = -Qi.inverse().toRotationMatrix();
+                // Jacobians from preintegrated velocity (beta)------------------------------------------------
+
+                jacobian_speedbias_i.block<3, 3>(O_V, O_V - O_V)  = -Qi.inverse().toRotationMatrix();
                 jacobian_speedbias_i.block<3, 3>(O_V, O_BA - O_V) = -dv_dba;
                 jacobian_speedbias_i.block<3, 3>(O_V, O_BG - O_V) = -dv_dbg;
 
-                jacobian_speedbias_i.block<3, 3>(O_BA, O_BA - O_V) = -Eigen::Matrix3d::Identity();
 
+                // Jacobians from preintegrated quaternion (gamma)---------------------------------------------
+
+                jacobian_speedbias_i.block<3, 3>(O_R, O_BG - O_V) = -Utility::Qright(
+                                                                            pre_integration->delta_q.inverse()
+                                                                            *
+                                                                            Qi.inverse() * Qj
+                                                                          ).bottomRightCorner<3, 3>()
+                                                                    * dq_dbg;
+
+                
+                // Jacobians from bias residuals --------------------------------------------------------------
+
+                jacobian_speedbias_i.block<3, 3>(O_BA, O_BA - O_V) = -Eigen::Matrix3d::Identity();
                 jacobian_speedbias_i.block<3, 3>(O_BG, O_BG - O_V) = -Eigen::Matrix3d::Identity();
+
+
+                // Applying the weightage----------------------------------------------------------------------
 
                 jacobian_speedbias_i = sqrt_info * jacobian_speedbias_i;
 
                 //ROS_ASSERT(fabs(jacobian_speedbias_i.maxCoeff()) < 1e8);
                 //ROS_ASSERT(fabs(jacobian_speedbias_i.minCoeff()) < 1e8);
             }
+
             if (jacobians[2])
             {
                 Eigen::Map<Eigen::Matrix<double, 15, 7, Eigen::RowMajor>> jacobian_pose_j(jacobians[2]);
                 jacobian_pose_j.setZero();
 
-                jacobian_pose_j.block<3, 3>(O_P, O_P) = Qi.inverse().toRotationMatrix();
 
-#if 0
-            jacobian_pose_j.block<3, 3>(O_R, O_R) = Eigen::Matrix3d::Identity();
-#else
-                Eigen::Quaterniond corrected_delta_q = pre_integration->delta_q * Utility::deltaQ(dq_dbg * (Bgi - pre_integration->linearized_bg));
-                jacobian_pose_j.block<3, 3>(O_R, O_R) = Utility::Qleft(corrected_delta_q.inverse()
-                                                                       * Qi.inverse() * Qj
+                // Jacobians from preintegrated position (alpha)-----------------------------------------------
+
+                jacobian_pose_j.block<3, 3>(O_P, O_P) =  Qi.inverse().toRotationMatrix();
+
+                
+                // Jacobians from preintegrated velocity (beta)------------------------------------------------
+                // ... none
+
+                
+                // Jacobians from preintegrated quaternion (gamma)---------------------------------------------
+
+                Eigen::Quaterniond corrected_delta_q = pre_integration->delta_q
+                                                       *
+                                                       Utility::deltaQ(dq_dbg * (Bgi - pre_integration->linearized_bg));
+
+                jacobian_pose_j.block<3, 3>(O_R, O_R) = Utility::Qleft(
+                                                                        corrected_delta_q.inverse()
+                                                                        *
+                                                                        Qi.inverse() * Qj
                                                                       ).bottomRightCorner<3, 3>();
-#endif
+
+
+                // Jacobians from bias residuals --------------------------------------------------------------
+                // ... none
+
+
+                // Applying the weightage----------------------------------------------------------------------
 
                 jacobian_pose_j = sqrt_info * jacobian_pose_j;
 
@@ -185,11 +237,26 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
                 Eigen::Map<Eigen::Matrix<double, 15, 9, Eigen::RowMajor>> jacobian_speedbias_j(jacobians[3]);
                 jacobian_speedbias_j.setZero();
 
+                // Jacobians from preintegrated position (alpha)-----------------------------------------------
+                // ... none
+
+
+                // Jacobians from preintegrated velocity (beta)------------------------------------------------
+
                 jacobian_speedbias_j.block<3, 3>(O_V, O_V - O_V) = Qi.inverse().toRotationMatrix();
 
-                jacobian_speedbias_j.block<3, 3>(O_BA, O_BA - O_V) = Eigen::Matrix3d::Identity();
 
+                // Jacobians from preintegrated quaternion (gamma)---------------------------------------------
+                // ... none
+
+
+                // Jacobians from bias residuals --------------------------------------------------------------
+
+                jacobian_speedbias_j.block<3, 3>(O_BA, O_BA - O_V) = Eigen::Matrix3d::Identity();
                 jacobian_speedbias_j.block<3, 3>(O_BG, O_BG - O_V) = Eigen::Matrix3d::Identity();
+
+
+                // Applying the weightage----------------------------------------------------------------------
 
                 jacobian_speedbias_j = sqrt_info * jacobian_speedbias_j;
 
